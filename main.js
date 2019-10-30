3b83c99a2110e904d000ec5e374faf64f3e5e902
Aum-S2 <sorasitdb7375@hotmail.com>
2019-10-30 15:59:49 +0700

    the scope chian

--- main.js
+++ main.js
@@ -11,19 +11,39 @@
 // console.log(a);
 
 
-// *functions, context, variable Environments
-function b() {
-  var myVar;
-  console.log(myVar);
-}
+
+// *****functions, context, variable Environments
+
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
+
+// function a() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   b();
+// }
+
+// var myVar = 1;
+// console.log(myVar);
+// a();
+// console.log(myVar);
+
+
+
+// ******The Scope Chian
 
 function a() {
+
+  function b() {
+    console.log(myVar);
+  }
+
   var myVar = 2;
-  console.log(myVar);
   b();
 }
 
 var myVar = 1;
-console.log(myVar);
 a();
-console.log(myVar);
+b();

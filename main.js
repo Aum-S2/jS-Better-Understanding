5e6600729bfe0b26bfc5623b0c136f10cea906d1
Aum-S2 <sorasitdb7375@hotmail.com>
2019-10-30 17:02:53 +0700

    Asynchronous Callbacks

--- main.js
+++ main.js
@@ -32,18 +32,50 @@
 
 
 
-// ******The Scope Chian
+// *****The Scope Chian
 
-function a() {
+// function a() {
 
-  function b() {
-    console.log(myVar);
-  }
+//   function b() {
+//     console.log(myVar);
+//   }
 
-  var myVar = 2;
-  b();
+//   var myVar = 2;
+//   b();
+// }
+
+// var myVar = 1;
+// a();
+// b();
+
+
+
+// *****Block scoping
+
+// if (a < b) {
+//   let c = true;
+// }
+
+
+
+// *****Asynchronous 
+// *Any events that outside of the engine get place into the queue and if execution stacks is empty and if javascript isnt working on anything else currently, it will process those events*
+// Asynchronous callbacks are possible in javascript. *but asynchronous part is really about What is happening outside the javascript engine *and javascript via this events loop, via this list of events that are happening, when its ready, will look at events and process them, but it does so synchronously
+
+// **Short - First all it does it just running its normal code, and when its all done, it will then go and look at the event queue, *AND if its already done, then its will continue to watch that event queue in the loop, the event loop. *Thats what thats called, the continuous check. *And the when it sees something, if there supposed to be a function, if theres a handle, if theres a listener thats supposed to run then that event appears in the event queue, IT WILL RUN
+
+function waitThreeSeconds() {
+  var ms = 5000 + new Date().getTime();
+  while (new Date() < ms){}
+  console.log('finished function');
 }
 
-var myVar = 1;
-a();
-b();
+function clickHandler() {
+  console.log('click Event!');
+}
+
+// *listen for the click event*
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');

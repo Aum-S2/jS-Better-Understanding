2ff9381cb7ef0aed5d11b0cd39aeeb9f74ff5ec1
Aum-S2 <sorasitdb7375@hotmail.com>
2019-10-31 13:40:59 +0700

    operators is functions

--- main.js
+++ main.js
@@ -64,18 +64,31 @@
 
 // **Short - First all it does it just running its normal code, and when its all done, it will then go and look at the event queue, *AND if its already done, then its will continue to watch that event queue in the loop, the event loop. *Thats what thats called, the continuous check. *And the when it sees something, if there supposed to be a function, if theres a handle, if theres a listener thats supposed to run then that event appears in the event queue, IT WILL RUN
 
-function waitThreeSeconds() {
-  var ms = 5000 + new Date().getTime();
-  while (new Date() < ms){}
-  console.log('finished function');
-}
+// function waitThreeSeconds() {
+//   var ms = 5000 + new Date().getTime();
+//   while (new Date() < ms){}
+//   console.log('finished function');
+// }
 
-function clickHandler() {
-  console.log('click Event!');
-}
+// function clickHandler() {
+//   console.log('click Event!');
+// }
 
-// *listen for the click event*
-document.addEventListener('click', clickHandler);
+// // *listen for the click event*
+// document.addEventListener('click', clickHandler);
 
-waitThreeSeconds();
-console.log('finished execution');
+// waitThreeSeconds();
+// console.log('finished execution');
+
+
+
+// Operators *Operators are functions
+
+var a = 4 + 7;
+var b = 5 < 2;
+var c = 6 - 3;
+var d = 9 > 8;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);

a80aa461f2b8de116a086c0de7ea8075fb8fb644
Aum-S2 <sorasitdb7375@hotmail.com>
2019-11-07 16:11:10 +0700

    arguments and spread

--- main02.js
+++ main02.js
@@ -73,9 +73,19 @@
 
 function greet(firstname, lastname, language) {
 
+  language = language || 'en';
+
+  if(arguments.length === 0) {
+    console.log('missing parameters');
+    console.log('-----------------');
+    return;
+  }
+
   console.log(firstname);
   console.log(lastname);
   console.log(language);
+  console.log(arguments);
+  console.log('arg 0: ' + arguments[0]);
   console.log('------------------');
 
 }
